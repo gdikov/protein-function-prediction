d614003db3454e2c35c4e817c0a8b86209c5694d
Atanas Mirchev <taimir93@gmail.com>
2016-11-05 09:09:52 +0100

    Add __init__ file for the preprocessing layer

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
+import draft.MoleculeMapLayer as mml
+import theano
+import lasagne
 
 if __name__ == "__main__":
-    # TODO: try out the layer here
-    pass
+    inputs = theano.tensor.tensor4()
+
+    network = lasagne.layers.InputLayer(shape=(None, 1, None, None), input_var=inputs)
+    network = mml.MoleculeMapLayer(network, active_or_inactive=0)
+    network.get_output_for(molecule_numbers01=[1, 2, 3])
