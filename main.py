472b17602cb7ec2c521f686e6e0f611232e440aa
Atanas Mirchev <taimir93@gmail.com>
2016-11-05 17:42:14 +0100

    Introduce further simplifications, remove obsolete
    Remove lava code throughout the preprocessing layer.

--- main.py
+++ main.py
@@ -6,5 +6,5 @@ if __name__ == "__main__":
     inputs = theano.tensor.tensor4()
 
     network = lasagne.layers.InputLayer(shape=(None, 1, None, None), input_var=inputs)
-    network = mml.MoleculeMapLayer(network, active_or_inactive=1)
-    network.get_output_for(molecule_numbers01=[1, 2, 3])
+    network = mml.MoleculeMapLayer(network)
+    network.get_output_for(molecule_ids=range(0, 10))
