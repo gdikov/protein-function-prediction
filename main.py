d3378fa931098ab3c4c8692bef3cd9803d151210
Atanas Mirchev <taimir93@gmail.com>
2016-11-06 18:40:56 +0100

    Save precomputed grids to file before visualization

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import theano
 import lasagne
 import time
+import numpy as np
+from os import path
 
 import draft.MoleculeMapLayer as mml
 import draft.MoleculeMapOld as old
 from visualizer.molview import MoleculeView
 
-if __name__ == "__main__":
+grid_file = "data/computed_grid.npy"
+
+
+def preprocess():
     batch_size = 1
     inputs = theano.tensor.tensor4()
     network = lasagne.layers.InputLayer(shape=(None, 1, None, None), input_var=inputs)
@@ -16,11 +21,18 @@ if __name__ == "__main__":
 
     start = time.time()
     grids = network.get_output_for(molecule_ids=range(0, batch_size)).eval()
-    print grids.shape
     # grids = network.get_output_for(molecule_numbers01=[range(0, batch_size), range(0, batch_size)]).eval()
-
     end = time.time()
     print(end - start)
+    np.save(grid_file, grids)
 
+
+def visualize():
+    grids = np.load(grid_file)
     viewer = MoleculeView(data={"potential": grids[0, 0], "density": grids[0, 1]}, info={"name": "test"})
     viewer.density3d()
+
+
+if __name__ == "__main__":
+    preprocess()
+    visualize()
