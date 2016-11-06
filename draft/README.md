2870ebad2f3c76d21e0676d293238fbc72076023
Atanas Mirchev <taimir93@gmail.com>
2016-11-06 10:41:45 +0100

    Comparison results between processing options

--- draft/README.md
+++ draft/README.md
@@ -4,3 +4,23 @@
 * **Step 2**: read the .memfiles and store their contents into Theano tensors.
 * **Step 3**: compute electrostatic potential and electron density at each grid point. Do this dynamically on the GPU,
 this step will be always executed.
+
+
+##### Some crunched numbers (on beetz10, no other load, 2GB gpu memory:)
+> old layer, 40 molecules, batch_size = 1 -> 36.5 to 36.7 seconds, ~ 200-300 MB max memory
+
+> old layer, 40 molecules, batch_size = 2 -> 24.5 seconds, up to ~ 1.400 GB memory
+
+> old layer, batch_size = 3 -> cannot fit into memory any more
+
+> new layer, 40 molecules, batch_size = 1 (not scan) -> 42.3 seconds, ~ 400 MB max memory
+
+> new layer, 40 molecules, batch_size = 2 -> 23.6 to 24 seconds, ~ 500 MB ? max memory
+
+> new layer, 39 molecules, batch_size = 3 -> 16 to 16.23 seconds, 1000 MB max memory
+
+> new layer, 40 molecules, batch_size = 4 -> 12.9 to 13.17 seconds, 1.4 GB max memory
+
+> new layer, 40 molecules, batch_size = 4, T.switch instead of elem-wise mult -> 12.8 seconds, 1.4 GB max memory
+
+> new layer, batch_size = 5 -> cannot fit into memory any more
--- main.py
+++ main.py
@@ -4,20 +4,25 @@ import time
 
 import draft.MoleculeMapLayer as mml
 import draft.MoleculeMapOld as old
-from visualizer.molview import MoleculeView
+# from visualizer.molview import MoleculeView
 
 if __name__ == "__main__":
-    start = time.time()
 
+    batch_size = 2
     inputs = theano.tensor.tensor4()
-
     network = lasagne.layers.InputLayer(shape=(None, 1, None, None), input_var=inputs)
-    network = mml.MoleculeMapLayer(network)
-    # network = old.MoleculeMapLayer(incoming=network, active_or_inactive=1)
-    grids = network.get_output_for(molecule_ids=range(0, 1)).eval()
-    # grids = network.get_output_for(molecule_numbers01=[[0],[0]]).eval()
+
+    network = mml.MoleculeMapLayer(network, batch_size=batch_size)
+    # network = old.MoleculeMapLayer(incoming=network, active_or_inactive=1, minibatch_size=batch_size)
+
+    start = time.time()
+    for i in range(0, 40 // batch_size):
+        grids = network.get_output_for(molecule_ids=range(0, batch_size)).eval()
+        # grids = network.get_output_for(molecule_numbers01=[range(0, batch_size), range(0, batch_size)]).eval()
 
     end = time.time()
     print(end - start)
-    viewer = MoleculeView(data={"potential": grids[0, 0], "density": grids[0, 1]}, info={"name": "test"})
-    viewer.density2d()
+
+
+    # viewer = MoleculeView(data={"potential": grids[0, 0], "density": grids[0, 1]}, info={"name": "test"})
+    # viewer.density3d()
