b54356f8a65a5031ef83347fc434e372669338da
Georgi Dikov <gvdikov@gmail.com>
2016-12-13 00:37:03 +0100

    implement the EnzymeMolDataFeeder

--- protfun/data_management/data_feed.py
+++ protfun/data_management/data_feed.py
@@ -3,6 +3,7 @@ import abc
 import colorlog as log
 import numpy as np
 import theano
+from os import path
 
 from protfun.data_management.data_manager import EnzymeDataManager
 
@@ -41,24 +42,24 @@ class EnzymeDataFeeder(DataFeeder):
                                               force_grids=False,
                                               force_split=False)
 
-    # NOTE: this method will be removed in the very near future. I need it for a moment.
-    def _iter_minibatches(self, mode='train'):
-        if mode == "train":
+    def _iter_minibatches(self, iter_mode='train'):
+        if iter_mode == "train":
             samples, labels = self.data_manager.get_training_set()
             data_dir = self.data_manager.dirs['data_train']
-        elif mode == "test":
+        elif iter_mode == "test":
             samples, labels = self.data_manager.get_test_set()
             data_dir = self.data_manager.dirs['data_test']
-        else:  # mode == "val"
+        elif iter_mode == "val":
             samples, labels = self.data_manager.get_validation_set()
             data_dir = self.data_manager.dirs['data_train']
+        else:
+            log.error("iter_mode can only be 'train', 'val' or 'test'")
+            raise ValueError
 
-        data_size = 0  # TODO: calculate the data size properly
-
-        # TODO: we need to do some sort of selection of the classes here
-        # TODO: because samples.keys() contains all leaf classes, and we probably don't need all of them
-        classes = samples.keys()
-        num_classes = len(classes)
+        represented_classes, data_sizes = \
+            map(list, zip(*[(cls, len(prots)) for cls, prots in samples.items() if len(prots) > 0]))
+        data_size = sum(data_sizes)
+        num_classes = len(represented_classes)
 
         effective_datasize = self.samples_per_class * num_classes
         if effective_datasize > data_size:
@@ -71,13 +72,15 @@ class EnzymeDataFeeder(DataFeeder):
                 minibatch_count += 1
 
         for _ in xrange(0, minibatch_count):
-            classes_in_minibatch = np.random.choice(classes, size=self.minibatch_size)
-            prots_in_minibatch = [np.random.choice(samples[cls][:self.samples_per_class]) for cls in
-                                  classes_in_minibatch]
+            classes_in_minibatch = np.random.choice(represented_classes,
+                                                    size=self.minibatch_size,
+                                                    replace=True)
+            prots_in_minibatch = [np.random.choice(samples[cls][:self.samples_per_class], size=1)
+                                  for cls in classes_in_minibatch]
 
-            # TODO: @georgi, can you make this work? next_targets should be a numpy array of the 1-hot target labels
-            # TODO: which are to be formed from the 'labels' variable
-            next_targets = list()
+            # labels are accessed at a fixed hierarchical depth of 3 counting from the root, e.g. 3.4.21.
+            # TODO: make it a variable and incoroporate the knowledge of all labels of depth < max_depth
+            next_targets = [labels[prot_code][2][0] for prot_code in prots_in_minibatch]
 
             next_samples = self._form_samples_minibatch(prot_codes=prots_in_minibatch, from_dir=data_dir)
             yield next_samples + next_targets
@@ -92,22 +95,42 @@ class EnzymesMolDataFeeder(EnzymeDataFeeder):
         super(EnzymesMolDataFeeder, self).__init__(minibatch_size, init_samples_per_class)
 
     def iterate_test_data(self):
-        for inputs in self._iter_minibatches(mode='test'):
+        for inputs in self._iter_minibatches(iter_mode='test'):
             yield inputs
 
     def iterate_train_data(self):
-        for inputs in self._iter_minibatches(mode='train'):
+        for inputs in self._iter_minibatches(iter_mode='train'):
             yield inputs
 
     def iterate_val_data(self):
-        for inputs in self._iter_minibatches(mode='val'):
+        for inputs in self._iter_minibatches(iter_mode='val'):
             yield inputs
 
     def _form_samples_minibatch(self, prot_codes, from_dir):
-        # TODO: load the memmaps from the given from_dir
-        # TODO: stack them into a numpy array
-        # TODO: return the array
-        raise NotImplementedError
+        assert len(prot_codes) == self.minibatch_size, \
+            "prot_codes must be of the same size as minibatch_size"
+        coords_tmp = []
+        charges_tmp = []
+        vdwradii_tmp = []
+        n_atoms = np.zeros((self.minibatch_size,), dtype=intX)
+        for i, prot_id in enumerate(prot_codes):
+            path_to_prot = path.join(from_dir, prot_id.upper())
+            coords_tmp.append(np.memmap(path.join(path_to_prot, 'coords.memmap'), mode='r', dtype=floatX))
+            charges_tmp.append(np.memmap(path.join(path_to_prot, 'charges.memmap'), mode='r', dtype=floatX))
+            vdwradii_tmp.append(np.memmap(path.join(path_to_prot, 'vdwradii.memmap'), mode='r', dtype=floatX))
+            n_atoms[i] = vdwradii_tmp[i].shape[0]
+
+        max_atoms = max(n_atoms)
+        coords = np.zeros((self.minibatch_size, max_atoms, 3))
+        charges = np.zeros((self.minibatch_size, max_atoms))
+        vdwradii = np.zeros((self.minibatch_size, max_atoms))
+
+        for i in range(self.minibatch_size):
+            coords[i, :n_atoms[i]] = coords_tmp[i]
+            charges[i, :n_atoms[i]] = charges_tmp[i]
+            vdwradii[i, :n_atoms[i]] = vdwradii_tmp[i]
+
+        return [coords, charges, vdwradii, n_atoms]
 
 
 class EnzymesGridFeeder(EnzymeDataFeeder):
@@ -115,15 +138,15 @@ class EnzymesGridFeeder(EnzymeDataFeeder):
         super(EnzymesGridFeeder, self).__init__(minibatch_size, init_samples_per_class)
 
     def iterate_test_data(self):
-        for inputs in self._iter_minibatches(mode='test'):
+        for inputs in self._iter_minibatches(iter_mode='test'):
             yield inputs
 
     def iterate_train_data(self):
-        for inputs in self._iter_minibatches(mode='train'):
+        for inputs in self._iter_minibatches(iter_mode='train'):
             yield inputs
 
     def iterate_val_data(self):
-        for inputs in self._iter_minibatches(mode='val'):
+        for inputs in self._iter_minibatches(iter_mode='val'):
             yield inputs
 
     def _form_samples_minibatch(self, prot_codes, from_dir):
