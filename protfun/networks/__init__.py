3f6fd4abfea0fc0e20557efa0b1854071b442931
Atanas Mirchev <taimir93@gmail.com>
2017-01-19 19:11:15 +0100

    Add a shallow network with dropout regularization

--- protfun/networks/__init__.py
+++ protfun/networks/__init__.py
@@ -1,15 +1,15 @@
-from protfun.networks.basic_convnet import basic_convnet
 from protfun.networks.single_trunk_network import single_trunk_network
 from protfun.networks.dense_net import dense_network
 from protfun.networks.resnet import resnet
 from protfun.networks.shallow_net import shallow_network
+from protfun.networks.regularized_net import regularized_net
 
 networks = {
-    "basic_convnet": basic_convnet,
     "single_trunk_network": single_trunk_network,
     "dense_network": dense_network,
     "resnet": resnet,
     "shallow_network": shallow_network,
+    "regularized_network": regularized_net
 }
 
 
