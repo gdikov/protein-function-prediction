b8b894f530301606c3cc55ca30630ba7377c418d
Atanas Mirchev <taimir93@gmail.com>
2017-03-12 14:00:52 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import sys
 import os
-# os.environ["THEANO_FLAGS"] = \
-#    "device=gpu2,lib.cnmem=1000,base_compiledir=~/.tiptoptwo"
 sys.setrecursionlimit(10000)
 # enable if you want to profile the forward pass
 # os.environ["CUDA_LAUNCH_BLOCKING"] = "1"
@@ -10,6 +8,6 @@ from protfun.models import train_enz_from_grids
 from protfun.config import get_config
 
 if __name__ == "__main__":
-    config_filepath = os.path.join(os.path.dirname(__file__), 'config.yaml')
+    config_filepath = os.path.join(os.path.dirname(__file__), 'experiments', 'example_gconfig.yaml')
     config = get_config(config_filepath)
-    train_enz_from_grids(config, model_name="regularized3")
+    train_enz_from_grids(config, model_name="my_example_model")
